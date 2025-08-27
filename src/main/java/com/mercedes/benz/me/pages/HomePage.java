7105e5ebeab1c48143cc17b6c12aec3304c6e4bc
Nikolai <n91056379@gmail.com>
2025-08-27 21:37:44 +0200

    add locator HEADER_INTEGRATION_ITEM_FSS_SEARCH_INPUT

--- src/main/java/com/mercedes/benz/me/pages/HomePage.java
+++ src/main/java/com/mercedes/benz/me/pages/HomePage.java
@@ -1,10 +1,15 @@
 package com.mercedes.benz.me.pages;
 
+import com.mercedes.benz.me.driver.SeleniumWebDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class HomePage {
 
     private final String HEADER_PRIVACY_POLICY_ICON = "//a[@data-test-id='header-privacy-policy']";
+
     private final By HEADER_INTEGRATION_ITEM_FSS_SEARCH_INPUT = By.xpath("//a[@data-test-id='header-integration-item-fss-search-input']");
 
-}
+   }
