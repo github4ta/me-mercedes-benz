e0a56a497aa4bb8bab9a100e2a31097d2bf91ee9
Alex Linnik <SanchezAnd3@gmail.com>
2025-08-31 20:03:11 +0300

    added a test to confirm the search box appears

--- src/test/java/com/mercedes/benz/me/AnyTest.java
+++ src/test/java/com/mercedes/benz/me/AnyTest.java
@@ -1,4 +1,22 @@
 package com.mercedes.benz.me;
 
+import com.mercedes.benz.me.driver.SeleniumWebDriver;
+import com.mercedes.benz.me.pages.BasePage;
+import com.mercedes.benz.me.pages.HomePage;
+import org.junit.jupiter.api.*;
+
 public class AnyTest {
+    @Test
+    @DisplayName("Checking for the presence of a search window")
+    public void test(){
+        boolean actual = new BasePage()
+                .openHomePage()
+                .clickInputSearch()
+                .isPresenceWindowSearch();
+        Assertions.assertTrue(actual);
+    }
+    @AfterEach
+    public void quit(){
+        SeleniumWebDriver.quitDriver();
+    }
 }
