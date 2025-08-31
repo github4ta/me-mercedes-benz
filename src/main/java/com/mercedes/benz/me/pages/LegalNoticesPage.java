b81b06f7e59b68c6035c67ba99b8b2bee7c88992
Nikolai <n91056379@gmail.com>
2025-08-31 15:29:42 +0200

    add LegalNoticesPage with locator and method

---
+++ src/main/java/com/mercedes/benz/me/pages/LegalNoticesPage.java
@@ -0,0 +1,20 @@
+package com.mercedes.benz.me.pages;
+
+import com.mercedes.benz.me.driver.SeleniumWebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LegalNoticesPage {
+
+    private final By LEGAL_NOTICES_TEXT = By.xpath("//h1[@class='title-hero__title'] ");
+
+    private final WebDriver driver;
+
+    public LegalNoticesPage() {
+        driver = SeleniumWebDriver.getDriver();
+    }
+
+    public String getTextLegalNotices() {
+        return driver.findElement(LEGAL_NOTICES_TEXT).getText();
+    }
+}
