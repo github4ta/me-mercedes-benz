eeed7cf99d096b5702993b9a49b2e0e6a2516d9f
Nikolai <n91056379@gmail.com>
2025-09-01 20:11:26 +0200

    add new locators and method

--- src/main/java/com/mercedes/benz/me/pages/HomePage.java
+++ src/main/java/com/mercedes/benz/me/pages/HomePage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 
 public class HomePage {
 
@@ -14,9 +16,9 @@ public class HomePage {
     private final By COPYRIGHT = By.cssSelector("li.owc-lower-footer-legal__license");
     private final By TERMS_OF_USE = By.cssSelector("li.owc-lower-footer-legal__item");
     private final By PRIVACY_NOTICE = By.cssSelector("li.owc-lower-footer-legal__item");
-    private final By PRIVATE_CUSTOMER = By.xpath("//div[@class='hp-header-ssr-user-menu__sublabel']");
+    private final By PRIVATE_CUSTOMER = By.xpath("(//div[@class='hp-icon-img wb-icon'])[4]");
     private final By PARENT_SELECTOR_PRIVATE_CUSTOMER = By.cssSelector("iam-user-menu-v3");
-    private final By TEXT_YOUR_MERCEDES_BENZ_ACCOUNT = By.cssSelector("data-test-id=user-menu-title");
+    private final By TEXT_YOUR_MERCEDES_BENZ_ACCOUNT = By.cssSelector("wb7-heading[data-test-id=user-menu-title]");
 
     private final WebDriver driver;
 
@@ -34,7 +36,7 @@ public class HomePage {
         SearchContext context = parentElement.getShadowRoot();
         WebElement element = context.findElement(COPYRIGHT);
         element.click();
-        return (HomePage) element;
+        return this;
     }
 
     public HomePage clickTermsOfUse() {
@@ -42,7 +44,7 @@ public class HomePage {
         SearchContext context = parentElement.getShadowRoot();
         WebElement element = context.findElement(TERMS_OF_USE);
         element.click();
-        return (HomePage) element;
+        return this;
     }
 
     public String getTextCopyright() {
@@ -72,10 +74,17 @@ public class HomePage {
         WebElement parentElement = driver.findElement(PARENT_SELECTOR_PRIVATE_CUSTOMER);
         SearchContext context = parentElement.getShadowRoot();
         return context.findElement(TEXT_YOUR_MERCEDES_BENZ_ACCOUNT).getText();
+
     }
 
-    public HomePage clickHeaderPrivacyPolicyIcon() {
+    public LegalNoticesPage clickHeaderPrivacyPolicyIcon() {
         driver.findElement(HEADER_PRIVACY_POLICY_ICON).click();
+        return new LegalNoticesPage();
+    }
+
+    public HomePage scrollSite() {
+        Actions actions = new Actions(driver);
+        actions.scrollByAmount(0, 900).perform();
         return this;
     }
 }
