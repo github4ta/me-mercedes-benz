ec80479214e95a7e6ea4366416ede87975cda8ab
Nikolai <n91056379@gmail.com>
2025-09-06 07:06:53 +0200

    add LegalNoticeTest page

---
+++ src/test/java/com/mercedes/benz/me/LegalNoticeTest.java
@@ -0,0 +1,26 @@
+package com.mercedes.benz.me;
+
+import com.mercedes.benz.me.driver.SeleniumWebDriver;
+import com.mercedes.benz.me.pages.BasePage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LegalNoticeTest {
+
+    @Test
+    @DisplayName("get legal notices text")
+    public void test() {
+        String actual = new BasePage()
+                .openHomePage()
+                .clickHeaderPrivacyPolicyIcon()
+                .getTextLegalNotices();
+
+        Assertions.assertEquals("Legal Notices", actual);
+    }
+
+    @Test
+    public void ternDawn() {
+        SeleniumWebDriver.quitDriver();
+    }
+}
