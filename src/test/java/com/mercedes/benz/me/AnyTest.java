1f5e905957b1909d03c69e16427f158e5cb90b75
Nikolai <n91056379@gmail.com>
2025-09-06 07:19:00 +0200

    refactor AnyTest

--- src/test/java/com/mercedes/benz/me/AnyTest.java
+++ src/test/java/com/mercedes/benz/me/AnyTest.java
@@ -1,46 +1,6 @@
 package com.mercedes.benz.me;
 
-import com.mercedes.benz.me.driver.SeleniumWebDriver;
-import com.mercedes.benz.me.pages.BasePage;
-import org.junit.jupiter.api.*;
 
 public class AnyTest {
 
-    @Test
-    @DisplayName("get legal notices text")
-    public void test() {
-        String actual = new BasePage()
-                .openHomePage()
-                .clickHeaderPrivacyPolicyIcon()
-                .getTextLegalNotices();
-
-        Assertions.assertEquals("Legal Notices", actual);
-    }
-
-    @Test
-    @DisplayName("get text YourMercedesBenzAccount")
-    public void test1(){
-        String actual = new BasePage()
-                .openHomePage()
-                .clickPrivateCustomer()
-                .getTextYourMercedesBenzAccount();
-
-        Assertions.assertEquals("Your Mercedes-Benz Account", actual);
-    }
-
-    @Test
-    @DisplayName("get text privacy notice")
-    public void test2(){
-        String actual = new BasePage()
-                .openHomePage()
-                .scrollSite()
-                .getTextPrivacyNotice();
-
-        Assertions.assertEquals("Privacy Notice", actual);
-    }
-
-    @Test
-    public void ternDawn() {
-        SeleniumWebDriver.quitDriver();
-    }
 }
