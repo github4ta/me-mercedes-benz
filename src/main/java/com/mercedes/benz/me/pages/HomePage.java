dfb93b1e9d478266eaf675c511e52e8524c96fc8
Nikolai <n91056379@gmail.com>
2025-09-14 14:04:34 +0200

    delete gray import

--- src/main/java/com/mercedes/benz/me/pages/HomePage.java
+++ src/main/java/com/mercedes/benz/me/pages/HomePage.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 
 public class HomePage {
